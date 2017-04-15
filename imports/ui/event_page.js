803c4937a98401802f67f268f744374ffa20bb2b
huyettcp <huyettcp@gmail.com>
2017-04-15 01:13:34 -0400

    title

--- imports/ui/event_page.js
+++ imports/ui/event_page.js
@@ -1,9 +1,39 @@
+import Highcharts from 'highcharts';
 import { Template } from 'meteor/templating';
 import { Teams } from '../api/teams.js';
 import { SportingEvents } from '../api/sportingEvents.js';
 import { Leagues } from '../api/leagues.js';
 
 import './event_page.html';
+
+function createHigh(eventData) {
+  $('#container').highcharts({
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: eventData.avgPrice
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten'
+      },
+    },
+    series: [
+      {
+        name: 'Jane',
+        data: [1, 0, 4]
+      }, {
+        name: 'John',
+        data: [5, 7, 3]
+      }
+    ]
+  });
+}
+
 
 
 Template.eventPage.onCreated(function() {
@@ -13,6 +43,7 @@ Template.eventPage.onCreated(function() {
   self.autorun(function() {
     var postId = FlowRouter.getParam('postId');
     self.subscribe('sportingEvents');
+
   //   var eventData = SportingEvents.find({}).count()
 		// console.log(eventData)  
   });
@@ -21,16 +52,14 @@ Template.eventPage.onCreated(function() {
 });
 
 Template.eventPage.rendered = function() {  
-	this.autorun(() => {
-    var testee = SportingEvents.findOne({})
-    console.log(testee)
-  });
-  //   this.autorun(function () {  
-  //   	var eventId = FlowRouter.getParam("id");
-		// var eventData = SportingEvents.find({}).count()
-		// console.log(eventData)
+    this.autorun(function () {  
+    	var eventId = FlowRouter.getParam("id");
+		var eventData = SportingEvents.findOne({})
 
-// })
+		console.log(eventData)
+       createHigh(eventData);
+
+})
 }
 
 
